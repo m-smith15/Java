599ada7e69e94ce06b45b8f605fc792dcf350e18
m-smith15 <mpsmith1215@gmail.com>
2022-07-31 17:25:11 -0400

    first Java commit!!!

---
+++ Fundamentals/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Shrimp Shrimpton");
+        System.out.println("I've hunted these waters for 300 years");
+        System.out.println("My spawning ground was the mariana trench");
+    }
+}
