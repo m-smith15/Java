14edfde8c828d2b38cbb5ab687d64e594363506a
m-smith15 <mpsmith1215@gmail.com>
2022-08-06 18:46:13 -0400

    weekend algo commit!

---
+++ algos/weekendalgosbaby.js
@@ -0,0 +1,12 @@
+function min(arr, toReturn) {
+    if(toReturn == 'value'){
+        return (Math.min(...arr));
+    }
+    else {
+        return arr.indexOf(Math.min(...arr), arr);
+    }
+}
+min([1,2,3,4,5], 'value') // => 1
+min([1,2,3,4,5], 'index') // => 0
+min([5,4,3,4,5], 'value') // => 3
+min([5,4,3,4,5], 'index') // => 2
