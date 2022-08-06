173eaebf74f775873f0713b0a24ee81bec780559
m-smith15 <mpsmith1215@gmail.com>
2022-08-06 00:20:40 -0400

    daikichi assignment updates!

--- daikichi_routes/src/main/java/com/example/demo/controllers/daikichiController.java
+++ daikichi_routes/src/main/java/com/example/demo/controllers/daikichiController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +19,20 @@ public class daikichiController {
 	@RequestMapping(value = "/daikichi/tomorrow", method=RequestMethod.GET)
 	public String tomorrowPage() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping(value = "/daikichi/travel/{city}", method=RequestMethod.GET)
+	public String fortuneCityPage(@PathVariable ("city") String city) {
+		return "Congrats, it looks like you'll be heading to " + city +  " in the near future";
+	}
+	
+	@RequestMapping(value = "/daikichi/lotto/{num}", method=RequestMethod.GET)
+	public String fortuneCityPage(@PathVariable ("num") Integer number) {
+		if(number % 2 == 0) {
+			return "EVEN - You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		else {			
+			return "ODD - You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
